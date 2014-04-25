a237ed5467c0edc600417f74d626db4ee5207ad1
smedisen <smedberg_5@hotmail.com>
2014-04-25 11:46:46 +0200

    fail loop

--- SNAKE.asm
+++ SNAKE.asm
@@ -4,64 +4,45 @@
  *  Created: 2014-04-25 08:02:33
  *   Author: b13petha
  */ 
- /*
+ 
+.list
 
-.DEF rTemp         = r16
-.DEF rDirection    = r23
+      rjmp      main
+main:
 
-.EQU NUM_COLUMNS   = 8
-.EQU MAX_LENGTH    = 25
-
-.DSEG
-matrix:   .BYTE 8
-snake:    .BYTE MAX_LENGTH+1
-
-.CSEG
-// Interrupt vector table
-.ORG 0x0000
-     jmp init // Reset vector
-//... fler interrupts
-.ORG INT_VECTORS_SIZE
-init:
-     // Sätt stackpekaren till högsta minnesadressen
-     ldi rTemp, HIGH(RAMEND)
-     out SPH, rTemp
-     ldi rTemp, LOW(RAMEND)
-     out SPL, rTemp*/
-
-
- /*.DSEG
-matrix:	.BYTE 8 
-
-.CSEG
-init:
-	ldi	r16 , 0
-
-loop:
-
-
-	out	PORTC0 , r16
-	out PORTD6 , r16
-
-	jmp loop*/
-                  // use them by their names rather than addresses (not fun).
-.list                  // We DO want to include the following code in our listing //D
-
-      rjmp      main       // You usually place these two lines after all your
-main:                  // directives. They make sure that resets work correctly.
-
-    ldi      r16,0xFF // LoaD Immediate. This sets r16 = 0xFF (255)
+    ldi      r16,0xFF
     out      DDRB,r16
 	out      DDRC,r16
 	out      DDRD,r16
 
+	ldi		r19 , 0
+	ldi		r20 , 127
+
 loop:
-	ldi	r16 , 0b00000001
-	ldi	r17 , 0b01000000
 
-    out      PORTC,r16
-    out      PORTD,r17         //1/2 /
+	subi	r19 , -1
+	cp		r19 , r20
+	brge	loop2
 
+loop1:
+	ldi	r16 , 0b00001000
+	ldi	r17 , 0b00000000
+	ldi r18 , 0b00000001
 
+    out     PORTC , r16
+    out     PORTD , r17
+	out		PORTB , r18
 
-    rjmp      loop
+	jmp loop
+
+loop2:
+	
+	ldi	r16 , 0b00000000
+	ldi	r17 , 0b00100000
+	ldi r18 , 0b00000100
+
+    out     PORTC , r16
+    out     PORTD , r17
+	out		PORTB , r18
+
+	jmp		loop
