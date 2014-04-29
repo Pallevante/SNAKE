f2c6d26a651d84b3568e778437ab780fc3303949
unknown <b13petha@SD30402.hs.local>
2014-04-29 07:54:45 +0200

    Fixat bat-skriptet. Laddar nu upp utan problem.

--- Debug/Upload.bat
+++ Debug/Upload.bat
@@ -1,3 +1,4 @@
 @echo off
 set /p comPort=Enter the port:%=%
-avrdude -C "C:\WinAVR-20100110\bin\avrdude.conf" -patmega328p -PcomcomPort -carduino -b115200 -Uflash:w:SNAKE.hex 
+avrdude -C "C:\WinAVR-20100110\bin\avrdude.conf" -patmega328p -Pcom%comPort% -carduino -b115200 -Uflash:w:SNAKE.hex 
+pause>nul
